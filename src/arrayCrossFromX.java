cf0693f640cc61e622379f4893d443fba7e62e04
egord <e.gordinski@gmail.com>
2022-03-22 11:22:26 +0200

    Update

--- src/arrayCrossFromX.java
+++ src/arrayCrossFromX.java
@@ -1,6 +1,13 @@
 public class arrayCrossFromX {
     public static void main(String[] args) {
         char sign = 'X';
+        char[][] array = new char[7][7];
 
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = array[i][sign];
+                System.out.println(array[i][j]);
+            }
+        }
     }
 }
